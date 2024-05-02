288e832d546880fe285f308eefed82bb0367dc57
Yogi Prayoga <yogikpiter@gmail.comgit --list>
2024-05-02 15:28:53 +0700

    feat: initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module tantangan-coding-challenge
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"tantangan-coding-challenge/data"
+)
+
+func main() {
+	// Hasil Grow Challenge
+	// arr := []int{1, 2, 3, 4}
+	// fmt.Println(data.Grow(arr))
+
+	// Hasil Reversed Challenge
+	fmt.Println(data.ReverseSeq(10))
+}
